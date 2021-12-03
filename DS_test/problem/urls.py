c133641cb3782b8df8bb0111734e6e26fde00b09
Mizuhara-yaguchi <55566473+Mizuhara-yaguchi@users.noreply.github.com>
2021-12-03 12:43:00 +0800

    首页

--- DS_test/problem/urls.py
+++ DS_test/problem/urls.py
@@ -4,4 +4,5 @@ from problem import views
 
 urlpatterns = [ 
     path('problem/', views.problem, name="problem"),
+    path('problem/pp<int:problem_id>/', views.detail, name='detail'),
 ]
--- DS_test/problem/views.py
+++ DS_test/problem/views.py
@@ -5,5 +5,12 @@ from django.contrib import auth
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib import messages
 
+from db_table.models import ProblemInfo
+
 def problem(request):
     return render(request, "problem/problem.html")
+
+def detail(request, problem_id):
+    problem = ProblemInfo.objects.get(ProblemId = problem_id)
+    print (problem_id)
+    return render(request, 'problem/detail.html', {'problem': problem})
