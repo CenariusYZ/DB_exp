e397725ef0888ede02fba151fa0ee094994b66f5
Mizuhara-yaguchi <55566473+Mizuhara-yaguchi@users.noreply.github.com>
2021-12-06 15:14:02 +0800

    比赛，搜索

--- DS_test/attempts/urls.py
+++ DS_test/attempts/urls.py
@@ -4,5 +4,5 @@ from attempts import views
 
 urlpatterns = [ 
     path('attempts/', views.attempts, name="attempts"),
-    path('attempts/page=<page_index>/', views.attempts_page, name="attempts_page"),
+    path('attempts/?s=<searchword>/page=<page_index>/', views.attempts_page, name="attempts_page"),
 ]
--- DS_test/match/urls.py
+++ DS_test/match/urls.py
@@ -4,4 +4,7 @@ from match import views
 
 urlpatterns = [ 
     path('match/', views.match, name="match"),
+    path('match/M<int:match_id>/', views.match_detail, name='match_detail'),
+    path('add/', views.add, name="add"),
+    path('addmatch/', views.addmatch, name="addmatch"),
 ]
